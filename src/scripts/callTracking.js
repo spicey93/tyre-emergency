3f8aa6b3ac40ddf1fe83ec6846268f1c244d0244
spicey93 <spicerdean@icloud.com>
2025-07-08 16:47:31 +0100

    more sources added

--- src/scripts/callTracking.js
+++ src/scripts/callTracking.js
@@ -21,17 +21,17 @@ function detectSource() {
     return "google_my_business";
   }
 
-  // if (utmSource === "gmb") {
-  //   return "gmb_bilsthorpe"
-  // }
+  if (utmSource === "gmb") {
+    return "gmb_bilsthorpe"
+  }
 
-  // if (utmSource === "gmb2") {
-  //   return "gmb_worksop"
-  // }
+  if (utmSource === "gmb2") {
+    return "gmb_worksop"
+  }
 
-  // if (utmSource === "gmb3") {
-  //   return "gmb_newark"
-  // }
+  if (utmSource === "gmb3") {
+    return "gmb_newark"
+  }
 
   if (/google\./.test(referrer)) {
     return "organic";
